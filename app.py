6296a9903fcbf848c4aee65d24c3ddb5aefe22f9
priyanka-riya <priyankaravichandran111@gmail.com>
2025-03-07 02:28:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from app_ui import main  # Import the main function from app_ui.py
+from app import main
+ # Import the main function from app_ui.py
 
 # Run the main function to launch the app
 if __name__ == "__main__":
